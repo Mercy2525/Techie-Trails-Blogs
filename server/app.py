8a7f2acb9827bd37d4c52520abde5d87392b7da8
sharonasaja <sharon.asaja@student.moringaschool>
2023-10-31 10:28:26 +0300

    modified models

--- server/app.py
+++ server/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
-
-
+from flask_migrate import Migrate
+from models import db,User,Blog,Comments
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///app.db'
+app.config['JSONIFY_PRETTYPRINT_REGULAR']= True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
