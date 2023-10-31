f823becc564e1edd9ae42a444724bfcf132635a8
benedict langat <benedictlangat2000@gmail.com>
2023-10-31 21:05:02 +0300

    Added client

---
+++ client/src/BlogForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import "./BlogForm.css"
+
+const BlogForm = ({ onSubmit }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ title, content, author });
+    setTitle('');
+    setContent('');
+    setAuthor('');
+    setFile(null);
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  return (
+    <div>
+      <h2>Create a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Content:</label>
+        <textarea
+          placeholder="Content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <label>Author:</label>
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <input
+        type="file"
+        onChange={handleFileChange}
+      />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
---
+++ client/src/HomePage.js
@@ -0,0 +1,30 @@
+// HomePage.js
+import React from 'react';
+import "./HomePage.css"
+//import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <div className='home'>
+      
+        <h1>Techie Trails Blog</h1>
+        <div>
+          <img
+            src="https://media.istockphoto.com/id/1480602468/photo/happy-asian-woman-wearing-headphones-and-sitting-on-sofa-listening-to-music-in-living-room-at.webp?b=1&s=170667a&w=0&k=20&c=L9_j9Bxk3nSFmFGNoyBI8VbkysXr3vMBOZydFgtGt38="
+            alt="Sample"
+          />
+        </div>
+        <h2>In "Harmonizing Code: The Art of Coding While Listening to Music," we explore the fascinating synergy between two seemingly unrelated worlds: coding and music.
+             <br/><br/>
+            Discover how the rhythmic patterns of your favorite tunes can enhance your coding flow and creativity. Uncover the science behind this creative combination and learn practical tips to optimize your coding experience while enjoying the melodies that inspire you.
+            <br/><br/>
+            Join us in this unique exploration of the harmony between code and music and unlock the potential for a more immersive and productive coding journey.</h2>
+            <h3>Author: Sharon Asaja</h3>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
