7f63ba212a59652d560faf8122856f43e426e2c8
Mercy <mercywmuriithi.mm@gmail.com>
2023-11-02 11:39:35 +0300

    retained the usable routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import Signup from './Signup';
 import HomePage from './HomePage';
-import BlogForm from './BlogForm';
 import Navbar from './Navbar';
 import About from './About';
 import Blogs from './Blogs';
 import BenBlogs from './BenBlogs';
-import Comments from './Comments';
 import BlogDisplay from './BlogDisplay';
 
 
@@ -18,27 +16,17 @@ function App() {
     <div>
           
       <Routes>
-        <Route element={<Navbar/>}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/blogform" element={<BlogForm />}/>
-          <Route path="/blog/:id" element={<BlogDisplay/>}/>
-
-          <Route path='/about'element={<About/>}/>
-          <Route path='/blogs' element={<Blogs/>} />
-         
-          <Route path='b.blogs' element={<BenBlogs/>} />
-          <Route path='blogform' element={<BlogForm/>} />
-          <Route path='comments' element={<Comments/>} />
-         
-         
-        </Route>
-        
+          <Route element={<Navbar/>}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/blog/:id" element={<BlogDisplay/>}/>
+              <Route path='/about'element={<About/>}/>
+              <Route path='/blogs' element={<Blogs/>} />
+              <Route path='b.blogs' element={<BenBlogs/>} />        
+          </Route> 
       </Routes>
     
      </div>
-
-  
   );
 }
 
